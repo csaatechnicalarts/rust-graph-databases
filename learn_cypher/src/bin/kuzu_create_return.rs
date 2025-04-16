477fe1b24ce06588e2152f73d19134dff7cc1028
Dennis Chua <dmcvocation@gmail.com>
2025-04-16 14:18:06 -0500

    Add notes for Docker + KuzuDB.

--- learn_cypher/src/bin/kuzu_create_return.rs
+++ learn_cypher/src/bin/kuzu_create_return.rs
@@ -17,13 +17,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ")?;
 
     let result = conn.query("
-        CREATE (u1: User {name: 'Carly', age: 31}), (u2: User {name: 'Keinichi', age: 47})
-        CREATE (u1)-[f: Follows {since: DATE('2025-03-25')}]->(u2)
+        CREATE (u1: User {name: 'Carly', age: 31}), 
+            (u2: User {name: 'Keinichi', age: 47}),
+            (u3: User {name: 'Akila', age: 25}),
+            (u4: User {name: 'Klaus', age: 38})
+        CREATE (u1)-[f1: Follows {since: DATE('2025-03-25')}]->(u2)
+        CREATE (u1)-[f2: Follows {since: DATE('2025-04-08')}]->(u3)
+        CREATE (u2)-[f3: Follows {since: DATE('2024-01-25')}]->(u3)
+        CREATE (u4)-[f4: Follows {since: DATE('2024-01-25')}]->(u1)
+        CREATE (u2)-[f5: Follows {since: DATE('2024-01-25')}]->(u4)
         CREATE (c1: City {name: 'Dallas', population: 1302638})
         CREATE (c2: City {name: 'Sendai', population: 2341000})
+        CREATE (c3: City {name: 'Cairo', population: 4493410})
+        CREATE (c4: City {name: 'Stuttgart', population: 632865})
         CREATE (c1)-[fc01: FriendshipCity {since: DATE('1997-08-01')}]->(c2)
+        CREATE (c3)-[fc02: FriendshipCity {since: DATE('1997-08-01')}]->(c4)
         CREATE (u1)-[l01: LivesIn]->(c1)
         CREATE (u2)-[l02: LivesIn]->(c2)
+        CREATE (u3)-[l03: LivesIn]->(c3)
+        CREATE (u4)-[l04: LivesIn]->(c4)
         RETURN *;    
     ")?;
 
